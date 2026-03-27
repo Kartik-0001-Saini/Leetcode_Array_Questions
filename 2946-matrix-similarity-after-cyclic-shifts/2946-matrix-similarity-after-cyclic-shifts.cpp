341c3de543df12afee7abe33d1b5497d95ea4987
Karitk <kartiksaini7988@gmail.com>
2026-03-27 14:47:17 +0530

    Time: 1 ms (39.75%), Space: 34 MB (5.59%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-
-    void rotateEven(vector<vector<int>>& res, int row, int col, int k){
+    void rotateEven(vector<vector<int>>& res, int row, int col, int k) {
         vector<int> ans;
-        for(int i=k; i<col; i++){
+        for (int i = k; i < col; i++) {
             ans.push_back(res[row][i]);
         }
-        for(int i=0; i<k; i++){
+        for (int i = 0; i < k; i++) {
             ans.push_back(res[row][i]);
         }
 
-        for(int i=0; i<col; i++){
+        for (int i = 0; i < col; i++) {
             res[row][i] = ans[i];
         }
     }
 
-    void rotateOdd(vector<vector<int>>& res, int row, int col, int k){
+    void rotateOdd(vector<vector<int>>& res, int row, int col, int k) {
         vector<int> ans;
-        for(int i=k; i<col; i++){
+
+        // right rotation = last k elements first
+        for (int i = col - k; i < col; i++) {
             ans.push_back(res[row][i]);
         }
-        for(int i=0; i<k; i++){
+        for (int i = 0; i < col - k; i++) {
             ans.push_back(res[row][i]);
         }
 
-        for(int i=0; i<col; i++){
+        for (int i = 0; i < col; i++) {
             res[row][i] = ans[i];
         }
     }
@@ -32,14 +33,14 @@ public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
         int row = mat.size(), col = mat[0].size();
         vector<vector<int>> res(mat.begin(), mat.end());
-        k = k%col;
-        if(k == 0) return true;
+        k = k % col;
+        if (k == 0)
+            return true;
 
-        for(int i=0; i<row; i++){
-            if(i%2==0){
+        for (int i = 0; i < row; i++) {
+            if (i % 2 == 0) {
                 rotateEven(res, i, col, k);
-            }
-            else{
+            } else {
                 rotateOdd(res, i, col, k);
             }
         }
