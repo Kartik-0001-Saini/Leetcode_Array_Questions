f4a2e6bfa607fcc4a65daf3b8caff83d3e0a7e2b
Karitk <kartiksaini7988@gmail.com>
2026-03-30 16:45:45 +0530

    Time: 0 ms (100%), Space: 9.2 MB (48.94%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> arr1(26, 0);
+        vector<int> arr2(26, 0);
+        
+        for(int i=0; i<s.size(); i++){
+            arr1[s[i] - 'a']++;
+        }
+
+        for(int i=0; i<t.size(); i++){
+            arr2[t[i] - 'a']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(arr1[i] != arr2[i]){
+                return i+'a';
+            }
+        }
+
+        return ' ';
+    }
+};
