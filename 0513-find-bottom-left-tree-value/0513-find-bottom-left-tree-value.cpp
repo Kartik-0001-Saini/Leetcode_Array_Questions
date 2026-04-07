33ac2ba7c541a00794556d14a39dd6c5a761120a
Karitk <kartiksaini7988@gmail.com>
2026-04-07 15:08:48 +0530

    Time: 4 ms (12.55%), Space: 28.1 MB (6.75%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -12,25 +12,31 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        if(root == NULL) return {};
+        if(root == NULL) return 0;
 
-        vector<vector<int>> res;
         queue<TreeNode*> q;
         q.push(root);
+        vector<vector<int>> res;
+        // So in this question what we are doing -->
+        // We known that from Level Order Traversal we store root from
+        // left to right.. And in this question we want the left element of last row of
+        // the tree. By LOT (Lev. Ord. Tvs.) we get the last most element in our vector
+        // at (n-1)th place and because we want left most it always present at 0th index.. 
 
         while(!q.empty()){
             int size = q.size();
             vector<int> ans;
+
             for(int i=0; i<size; i++){
                 TreeNode* node = q.front();
                 q.pop();
                 ans.push_back(node->val);
-                
-                if(node->left) q.push(node->left);
 
+                if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
-            res.push_back(move(ans));
+
+            res.push_back(ans);
         }
 
         int n = res.size();
